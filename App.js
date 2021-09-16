818c85782535acbfe742cee96b833c139c80daa2
Genesis Murillo <genesis.mrll@gmail.com>
2021-09-16 08:12:57 -0600

    Remove unused vars

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createStackNavigator } from "react-navigation-stack";
